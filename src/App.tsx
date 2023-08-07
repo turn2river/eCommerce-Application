3b726e1f51162d5fc02c54ff330b9d063e85cfaa
alexey.krylov <turn2river@gmail.com>
2023-08-07 18:11:23 +0300

    feat: add folder structure (EC-2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.scss'
+import { Header } from './components/Header/Header'
 
 function App() {
 
   return (
-    <div className="app">
+    <>
+      <Header />
+      <div className="app">
       This is our react application
-    </div>
+      </div>
+    </>
   )
 }
 
