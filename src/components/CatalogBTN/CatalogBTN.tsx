d192338390e716ca5b6a8ad4676ddd65b785a237
Laevus92 <laevus@yandex.ru>
2023-08-09 19:07:26 +0300

    fix(catalogbtn.tsx): change type button on dropdown element

--- src/components/CatalogBTN/CatalogBTN.tsx
+++ src/components/CatalogBTN/CatalogBTN.tsx
@@ -1,14 +1,47 @@
+import { useState } from 'react'
 import classes from './CatalogBTN.module.scss'
+import classitem from '../CatalogItem/CatalogItem.module.scss'
+import { CatalogItem } from '../CatalogItem/CatalogItem.tsx'
 
-interface ICatalogBTN extends React.ComponentPropsWithRef<'button'> {
+interface ICatalogBTN extends React.ComponentPropsWithRef<'div'> {
   children: string
 }
 
 export const CatalogBTN = ({ children, ...props }: ICatalogBTN): JSX.Element => {
+  const [catalogButtonStatus, setCatalogButtonStatus] = useState(false)
+  const [activeElement, setActiveElement] = useState<number | null>(null)
+
+  const categories = ['For men', 'For women']
+
+  const clickOnCatalogBTN = (): void => {
+    setCatalogButtonStatus((active) => !active)
+  }
+
+  const clickOnCatalogItem = (index: number): void => {
+    setActiveElement(index)
+    console.log(activeElement)
+  }
+
+  const buttonClassName = catalogButtonStatus ? `${classes.button} ${classes.button__active}` : classes.button
+
   return (
-    <button className={classes.button} {...props}>
-      <div className={`${classes['button-icon']}`}></div>
-      <span className={`${classes['button-caption']}`}>{children}</span>
-    </button>
+    <div className={classes.container}>
+      <div className={buttonClassName} {...props} onClick={clickOnCatalogBTN}>
+        <div className={classes['button-icon']}></div>
+        <span className={classes['button-caption']}>{children}</span>
+      </div>
+      {catalogButtonStatus ? (
+        <div className={classes.container__wrapper}>
+          {categories.map((category, index) => (
+            <CatalogItem
+              key={index}
+              className={`${activeElement === index ? `${classitem.item} ${classitem.item__active}` : classitem.item}`} // Adjust the class names
+              onClick={(): void => clickOnCatalogItem(index)}>
+              {category}
+            </CatalogItem>
+          ))}
+        </div>
+      ) : null}
+    </div>
   )
 }
