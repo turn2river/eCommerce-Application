e248c8e958c5acf6e69a0420aa8d2bf6acf51378
Laevus92 <laevus@yandex.ru>
2023-08-09 11:41:40 +0300

    feat(downloadbtn.tsx): add active style for download button (#EC-57)

--- src/components/DownloadBTN/DownloadBTN.tsx
+++ src/components/DownloadBTN/DownloadBTN.tsx
@@ -4,9 +4,10 @@ interface IDownloadBTN extends React.ComponentPropsWithRef<'button'> {
   children: string
 }
 
-export const DownloadBTN = ({ children }: IDownloadBTN): JSX.Element => {
+export const DownloadBTN = ({ children, ...props }: IDownloadBTN): JSX.Element => {
+  console.log(props)
   return (
-    <button className={classes.button}>
+    <button className={classes.button} {...props}>
       <span className={`${classes['button-caption']}`}>{children}</span>
     </button>
   )
