f388d5dd63778595bc5557336ac26864f0495655
Laevus92 <laevus@yandex.ru>
2023-08-10 16:37:10 +0300

    fix(app.tsx): remove button elements from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.scss'
-import { CatalogButton, Header, DownloadButton } from './components'
+import { Header } from './components'
 
 export function App(): JSX.Element {
   return (
     <React.Fragment>
       <Header />
-      <DownloadButton>Download</DownloadButton>
-      <CatalogButton>Catalog</CatalogButton>
       <div className="app">This is our react application</div>
     </React.Fragment>
   )
