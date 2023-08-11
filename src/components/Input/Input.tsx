e4df86050b733e086e45cedb068c13b9c7295f5f
Denis Boldyrev <laevus@yandex.ru>
2023-08-11 22:42:10 +0300

    refactor(input.tsx): add ternary operator (#EC-58)

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,21 +2,13 @@ import { IInput } from '../../models/InputInterface'
 import { container, input_label, input, input_alert } from './Input.module.scss'
 
 export const Input = ({ id, label, type, placeholder, value, disabled, alert, onChange }: IInput): JSX.Element => {
-  let className: string = ''
-
-  if (alert) {
-    className = `${input} ${input_alert}`
-  } else {
-    className = input
-  }
-
   return (
     <div className={container}>
       <label htmlFor={id} className={input_label}>
         {label}
       </label>
       <input
-        className={className}
+        className={alert ? `${input} ${input_alert}` : input}
         id={id}
         type={type}
         placeholder={placeholder}
