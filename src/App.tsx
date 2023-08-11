eb1990e8fa399085193331e184813ff4d7ad59d3
Laevus92 <laevus@yandex.ru>
2023-08-11 13:15:54 +0300

    feat(input.module.scss): add pseudo-element

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
-import { Header } from './components'
+import { Header, Input } from './components'
 
 export function App(): JSX.Element {
+  const [value, setValue] = useState('')
+
+  function inputHandler(e: React.ChangeEvent<HTMLInputElement>): void {
+    setValue(e.target.value)
+  }
+
   return (
     <React.Fragment>
       <Header />
+      <Input
+        id="email"
+        label="Input your email address*"
+        placeholder="<E-Mail>"
+        value={value}
+        onChange={inputHandler}
+      />
       <div className="app">This is our react application</div>
     </React.Fragment>
   )
