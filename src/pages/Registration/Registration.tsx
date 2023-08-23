d804fae9ec54647aba7a8e0a6f265a443231e010
alexey.krylov <turn2river@gmail.com>
2023-08-23 10:42:47 +0300

    feat(approutes.ts): redirect to main from login route after success sign in

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,12 +1,16 @@
 import { Fragment } from 'react'
+import { NavLink } from 'react-router-dom'
 import { RegistrationForm } from '../../components'
-import { container } from './Registration.module.scss'
+import { container, sign_in_link } from './Registration.module.scss'
 
 export const Registration = (): JSX.Element => {
   return (
     <Fragment>
       <div className={container}>
         <RegistrationForm />
+        <NavLink className={sign_in_link} to="/login">
+          Already have an account? Sign in here!
+        </NavLink>
       </div>
     </Fragment>
   )
