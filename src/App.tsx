f4d91dbd7dd689091ce795fceeed330a8aba9e94
JuliaBel5 <juliabel5@gmail.com>
2023-09-01 19:13:06 +0300

    feat: add  get products by category ID service

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ProductsService } from './services/ProductsQueryService'
 import { AnonTokensStorage } from './store/anonTokensStorage'
 import { GetCustomerByTokenService } from './services/GetCustomerByTokenService'
 import { CustomerTokensStorage } from './store/customerTokensStorage'
+import { GetProductsByCategoryIdService } from './services/GetProductsByCategoryIdService'
 
 export function App(): JSX.Element {
   return (
@@ -25,9 +26,14 @@ const anonTokensStorage = AnonTokensStorage.getInstance()
 const anonUserAuthToken = anonTokensStorage.getLocalStorageAnonAuthToken()
 const customerTokensStorage = new CustomerTokensStorage()
 const customerToken = customerTokensStorage.getLocalStorageCustomerAuthToken()
+
+const categoryProducts = new GetProductsByCategoryIdService()
+
 if (anonUserAuthToken) {
   const catalogue = new ProductsService()
   catalogue.getProducts(anonUserAuthToken, 6, 2)
+  const id = 'c3bbd3e2-ba78-4a21-9de1-e5c0ccdefc38' // это женские нишевые ароматы, просто пример
+  categoryProducts.getProductsByCategoryId(anonUserAuthToken, id)
 }
 
 if (customerToken) {
---
+++ src/services/GetProductsByCategoryIdService.ts
@@ -0,0 +1,102 @@
+import axios from 'axios'
+
+export class GetProductsByCategoryIdService {
+  public async getProductsByCategoryId(token: string, id: string): Promise<CategoryProductsData> {
+    const url = `https://api.europe-west1.gcp.commercetools.com/parfumerie/product-projections/search?filter=categories.id:"${id}"`
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+    const response = await axios.get(url, { headers })
+    console.log(response.data.results)
+    return response.data
+  }
+}
+
+interface CategoryProductsData {
+  limit: number
+  offset: number
+  count: number
+  total: number
+  results: ProductResult[]
+  facets: Record<string, unknown>
+}
+
+interface ProductResult {
+  id: string
+  version: number
+  productType: {
+    typeId: string
+    id: string
+  }
+  name: {
+    [locale: string]: string
+  }
+  description: {
+    [locale: string]: string
+  }
+  categories: {
+    typeId: string
+    id: string
+  }[]
+  categoryOrderHints: Record<string, unknown>
+  slug: {
+    [locale: string]: string
+  }
+  metaTitle: {
+    [locale: string]: string
+  }
+  metaDescription: {
+    [locale: string]: string
+  }
+  variants: ProductVariant[]
+  masterVariant: ProductVariant
+  searchKeywords: Record<string, unknown>
+  hasStagedChanges: boolean
+  published: boolean
+  key: string
+  priceMode: string
+  createdAt: string
+  lastModifiedAt: string
+}
+
+interface ProductVariant {
+  attributes: {
+    name: string
+    value: number[]
+  }[]
+  assets: unknown[]
+  images: {
+    url: string
+    dimensions: {
+      w: number
+      h: number
+    }
+  }[]
+  prices: ProductPrice[]
+  key: string
+  id: number
+}
+
+interface ProductPrice {
+  id: string
+  value: {
+    type: string
+    currencyCode: string
+    centAmount: number
+    fractionDigits: number
+  }
+  key: string
+  discounted: {
+    value: {
+      type: string
+      currencyCode: string
+      centAmount: number
+      fractionDigits: number
+    }
+    discount: {
+      typeId: string
+      id: string
+    }
+  }
+}
